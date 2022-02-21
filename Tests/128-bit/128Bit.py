646e0efad042552ce089ea1577537d8023c110bc
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 01:02:07 -0800

    12

---
+++ Tests/128-bit/128Bit.py
@@ -0,0 +1,7 @@
+x == 2.0
+y = 128.0
+print(x ** y)
+print("If the answer is not abbreviated, you are running on a 128 bit or higher processor. If not, you are running on 127 bit or lower processor.") 
+break
+# File version: 1 (2022, Monday, February 21st at 1:03 am)
+# Line count (including blank lines and compiler line): 8
